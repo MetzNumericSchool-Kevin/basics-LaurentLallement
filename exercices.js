8fb43ddc2463c6df4a0f457c27c866823f9dd0d9
LaurentLallement <lallela95@gmail.com>
2025-02-05 11:54:27 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -3,3 +3,8 @@ const nbPotions = 27;
 const potionPrice = 50.50;
 const shopIsOpen = true;
 
+    if (shopIsOpen) {
+        console.log("Bienvenue dans la boutique " + SHOP_NAME + " Aventurier ! 🎉");
+    } else console.log("La boutique " + SHOP_NAME + " est fermée, revenez plus tard Aventurier ! 😴");
+
+
