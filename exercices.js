c7a9ad53de3784e5e47726a1beb3eeab31e00607
LaurentLallement <lallela95@gmail.com>
2025-02-06 16:30:27 +0100

    Affichage conditionnel avec switch

--- exercices.js
+++ exercices.js
@@ -1,10 +1,72 @@
+const readline = require('readline');
+
 const SHOP_NAME = "Magic MNS";
+const SORCERER_NAME = "Archibald 🧙‍♂️";
 const nbPotions = 27;
 const potionPrice = 50.50;
 const shopIsOpen = true;
 
-    if (shopIsOpen) {
-        console.log("Bienvenue dans la boutique " + SHOP_NAME + " Aventurier ! 🎉");
-    } else console.log("La boutique " + SHOP_NAME + " est fermée, revenez plus tard Aventurier ! 😴");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+if (shopIsOpen) {
+    console.log("Bienvenue dans la boutique " + SHOP_NAME + " Aventurier ! 🎉");
+} else {
+    console.log("La boutique " + SHOP_NAME + " est fermée, revenez plus tard Aventurier ! 😴");
+    rl.close();
+    process.exit();
+}
 
+function askQuestion() {
+    rl.question(
+        "\nBienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔\n" +
+        "1. Le nom de la boutique\n" +
+        "2. Le nom du Sorcier\n" +
+        "3. Le prix d'une potion de soin\n" +
+        "4. La quantité de potions de soin en stock\n" +
+        "5. Quitter la boutique\n" +
+        "Choisissez un nombre : ",
+        (userChoice) => {
+            let userChoiceInt = parseInt(userChoice);
+            userChoiceInt = isNaN(userChoiceInt) ? 0 : userChoiceInt;
+
+            switch (userChoiceInt) {
+                case 1:
+                    console.log("Le nom de la boutique : " + SHOP_NAME);
+                    break;
+                case 2:
+                    console.log("Mon nom de sorcier est : " + SORCERER_NAME);
+                    break;
+                case 3: {
+                    const format = new Intl.NumberFormat('fr-FR', {
+                        style: 'currency',
+                        currency: 'EUR'
+                    });
+                    console.log("Le prix d'une potion de soin est de : " + format.format(potionPrice));
+                }
+                    break;
+                case 4: {
+                    const format = new Intl.NumberFormat('fr-FR', {
+                        style: 'decimal',
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 0
+                    });
+                    console.log("La quantité de potion de soin en stock : " + format.format(nbPotions));
+                }
+                    break;
+                case 5:
+                    console.log("Merci de ta visite, aventurier ! À bientôt ! 👋");
+                    rl.close();
+                    return;
+                default:
+                    console.log("Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕");
+            }
+
+            setTimeout(askQuestion,1500); // Redemander après une réponse
+        }
+    );
+}
+
+askQuestion();
